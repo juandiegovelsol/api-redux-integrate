14008adb4aaadec8b9cdfb2a2c6eef9f48e52ab3
Juan <juandiegovelsol@gmail.com>
2023-05-04 18:07:58 -0500

    scss updates

--- src/features/Login/Login.jsx
+++ src/features/Login/Login.jsx
@@ -28,20 +28,28 @@ const Login = () => {
       <section className="login__container">
         <h1 className="login__title">FAVS LOGIN</h1>
         <Form className="login__form" onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Group className="login__mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              className="login__input"
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="login__mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              className="login__input"
+            />
           </Form.Group>
 
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" className="login__button">
             Submit
           </Button>
         </Form>
